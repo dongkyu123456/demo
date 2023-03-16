ec86cd88146e26417e923429266d43ac42c5e58c
dongkyu123456 <121007226+dongkyu123456@users.noreply.github.com>
2023-03-16 15:32:17 +0900

    dd

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping({"/",""})
+public class MainController {
+   @GetMapping("/terran")
+    private String terran(){
+        return "terran";
+    }
+
+    @GetMapping("/protoss")
+    public String protoss() {
+        return "protoss";
+    } 
+
+    @GetMapping("/zerg")
+    public String zerg() {
+        return "zerg";
+    } 
+
+}
